d8c7810a4c4ee8d41dfe79e6c2ec8f1d032bdfed
rithikaaa03 <rithika092917@gmail.com>
2022-10-15 19:35:42 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>This is an About component
+    <p>lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quae.
+    </p>
+    </div>
+  )
+}
